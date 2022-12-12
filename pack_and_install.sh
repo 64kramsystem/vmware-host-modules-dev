6d852ce9bbf8b9c366467a3ebe272706c5493ca9
Saverio Miroddi <saverio.pub2@gmail.com>
2022-12-13 00:26:08 +0100

    Add utility script pack_and_install.sh

---
+++ pack_and_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o errtrace
+shopt -s inherit_errexit
+
+make tarballs
+sudo chown root: *.tar
+
+sudo mv -vf *.tar /usr/lib/vmware/modules/source/
+
+sudo vmware-modconfig --console --install-all
